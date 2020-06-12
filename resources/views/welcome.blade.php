f314f2eb335c31f3a6ea3f9c52acd2f75ad78f27
mochasef99 <mochamadasef99@gmail.com>
2020-06-12 20:01:10 +0700

    FESA - Merubah Tampilan Awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pendaftaran MABA UMY
                 </div>
 
                 <div class="links">
