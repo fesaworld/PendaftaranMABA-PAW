862ceaf821bc13eb950cb6e287dfbc7ef13dccb8
mochasef99 <mochamadasef99@gmail.com>
2020-06-12 22:06:50 +0700

    FESA - Edit Form Registrasion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Biodata;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\File;
 
 class RegisterController extends Controller
 {
@@ -40,6 +48,13 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+    public function showRegistrationForm()
+    {
+        $biodata = Biodata::all();
+ 
+        return view('auth.register', compact('biodata'));
+    }
+
     /**
      * Get a validator for an incoming registration request.
      *
@@ -68,5 +83,91 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function register(Request $request)
+    {
+ 
+        $validator = Validator::make($request->all(), 
+        [
+            'email' => 'required|string|email|max:191|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'nama' => 'required|string|max:100',
+            'nisn' => 'required|string|max:45',
+            'tempat_lahir' => 'required|string|max:45',
+            'tanggal_lahir' => 'required',
+            'no_telp' => 'required|string|max:45',
+            'alamat' => 'required',
+            'un_average' => 'required|string|max:45',
+            'no_ijazah' => 'required|string|max:45',
+            'foto' => 'mimes:pdf,jpeg,png,jpg|max:2048',
+            'berkas' => 'mimes:pdf,jpeg,png,jpg|max:2048',
+ 
+        ],
+ 
+        $messages = 
+        [
+            'email.required' => 'E-Mail tidak boleh kosong !',
+            'password.required' => 'Password tidak boleh kosong',
+            'nama.required' => 'Nama tidak boleh kosong!',
+            'nisn.required' => 'NISN tidak boleh kosong!',
+            'tempat_lahir.required' => 'Tempat lahir tidak boleh kosong!',
+            'tanggal_lahir.required' => 'Tanggal lahir tidak boleh kosong!',
+            'no_telp.required' => 'No.Telpon tidak boleh kosong!',
+            'alamat.required' => 'Alamat tidak boleh kosong!',
+            'un_average.required' => 'Nem tidak boleh kosong!',
+            'no_ijazah.required' => 'No.Ijazah tidak boleh kosong!',
+            'foto.image' => 'Format file tidak mendukung! Gunakan jpg, jpeg, png, gif atau pdf.',
+            'foto.max' => 'Ukuran file terlalu besar, maksimal file 2Mb !',
+            'berkas.image' => 'Format file tidak mendukung! Gunakan jpg, jpeg, png, gif atau pdf.',
+            'berkas.max' => 'Ukuran file terlalu besar, maksimal file 2Mb !',
+        ]);     
+ 
+        if($validator->fails())
+        {
+        return back()->withErrors($validator)->withInput();  
+        }
+ 
+        //Table Users
+        $user = new User;
+        $user->email = Input::get('email');
+        $user->password = Hash::make(Input::get('password'));
+        $user->level = 1;
+        $user->save();
+ 
+        //Table Biodata 
+        $user_id = $user->id;
+        $biodata = new Biodata;
+        $biodata->user_id = $user_id;
+        $biodata->nama = Input::get('nama');
+        $biodata->nis = Input::get('nisn');
+        $biodata->tempat_lahir = Input::get('tempat_lahir');
+        $biodata->tanggal_lahir = Input::get('tanggal_lahir');
+        $biodata->telp = Input::get('telp');
+        $biodata->alamat = Input::get('alamat');
+        $biodata->nem = Input::get('un_average');
+        $biodata->no_ijazah = Input::get('no_ijazah');
+        if($file = $request->hasFile('foto')) 
+        {
+            $namaFile = $user->id;
+            $file = $request->file('foto') ;
+            $fileName = $namaFile.'_'.$file->getClientOriginalName() ;
+            $destinationPath = public_path().'/images/' ;
+            $file->move($destinationPath,$fileName);
+            $biodata->url_foto = $fileName ;
+        }
+        if($file = $request->hasFile('berkas')) 
+        {
+            $namaFile = $user->id;
+            $file = $request->file('berkas') ;
+            $fileName = $namaFile.'_'.$file->getClientOriginalName() ;
+            $destinationPath = public_path().'/berkas/' ;
+            $file->move($destinationPath,$fileName);
+            $biodata->url_foto = $fileName ;
+        }
+        $biodata->save();
+     
+ 
+        return redirect()->back()->with('Success', 'Registrasi Anda telah berhasil!. Silakan login dengan menggunakan email dan password Anda.');
     }
 }
